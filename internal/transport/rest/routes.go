68b88a17c1ac9cbdcf0898d247ae1a423382654a
hitendrarao <hitendra.rao@droom.in>
2022-03-07 03:07:35 +0530

    get and post method added in routes

--- internal/transport/rest/routes.go
+++ internal/transport/rest/routes.go
@@ -3,5 +3,24 @@ package rest
 import "net/http"
 
 func (h *Handlers) setupRoutes(router *http.ServeMux) {
-	router.HandleFunc("/hello", h.hello())
+	router.HandleFunc("/hello", get(h.hello()))
+	router.HandleFunc("/post", post(h.hello()))
+}
+
+func allowMethod(h http.HandlerFunc, method string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if method != r.Method {
+			http.Error(w, "405 method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		h(w, r)
+	}
+}
+
+func get(h http.HandlerFunc) http.HandlerFunc {
+	return allowMethod(h, "GET")
+}
+
+func post(h http.HandlerFunc) http.HandlerFunc {
+	return allowMethod(h, "POST")
 }
